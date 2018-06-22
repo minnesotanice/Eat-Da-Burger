19eb26fbaf9629c873a07c518a33a68039df4d73
Steph Japs <stephjaps@gmail.com>
2018-06-21 23:25:53 -0500

    syntax of schema updated

--- db/schema.sql
+++ db/schema.sql
@@ -2,8 +2,8 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers(
-	id int NOT NULL AUTO_INCREMENT,
+	id INT AUTO_INCREMENT NOT NULL,
 	burger_name varchar(255) NOT NULL,
 	eaten BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
+	PRIMARY KEY(id)
 );
